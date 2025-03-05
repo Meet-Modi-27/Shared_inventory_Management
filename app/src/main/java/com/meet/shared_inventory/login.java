d5f55d5d8a1612c7d22e00aac69b7aca630638d4
Meet Modi <meetmodi2705@gmail.com>
2025-03-05 13:42:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/meet/shared_inventory/login.java
@@ -0,0 +1,24 @@
+package com.meet.shared_inventory;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
